6d4a04582ea0de22daec96d1716551d117249dc7
Kevin SuTJ <shengsu15@gmail.com>
2022-08-05 14:17:01 +0800

    serialLazy

--- app/src/main/java/cn/neday/graduates/repository/Score.kt
+++ app/src/main/java/cn/neday/graduates/repository/Score.kt
@@ -2,7 +2,7 @@ package cn.neday.graduates.repository
 
 import cn.neday.graduates.MMKV_Crypt_Key
 import cn.neday.graduates.repository.serialize.MMKVOwner
-import cn.neday.graduates.repository.serialize.serial
+import cn.neday.graduates.repository.serialize.serialLazy
 import com.dylanc.longan.internalFileDirPath
 import com.tencent.mmkv.MMKV
 
@@ -33,61 +33,61 @@ object Score : MMKVOwner {
         )
     }
 
-    var month by serial(96) // 剩余月
-    var time by serial(230) // 剩余时间
+    var month by serialLazy(96) // 剩余月
+    var time by serialLazy(230) // 剩余时间
 
-    var isPosition by serial(false) // 职位 0/1
-    var position by serial(0) // 职位
+    var isPosition by serialLazy(false) // 职位 0/1
+    var position by serialLazy(0) // 职位
 
-    var isHouse by serial(false) // 房产 0/1
-    var house by serial(0) // 房产
+    var isHouse by serialLazy(false) // 房产 0/1
+    var house by serialLazy(0) // 房产
 
-    var isCar by serial(false) // 汽车 0/1
-    var car by serial(0) // 汽车
+    var isCar by serialLazy(false) // 汽车 0/1
+    var car by serialLazy(0) // 汽车
 
     //TODO: i++
-    var purity by serial(100) // 分数含金量
-    var money by serial(2000) // 金钱
-    var stock by serial(0) // 股票
-    var career by serial(1) // 事业指数
-    var love by serial(1) // 爱情指数
-    var indexHouse by serial(100) // 房价指数
-    var income by serial(2200) // 月净收益
-    var communicationMonthly by serial(0) // 月净交际
-    var happyMonthly by serial(-2) // 月净快乐
+    var purity by serialLazy(100) // 分数含金量
+    var money by serialLazy(2000) // 金钱
+    var stock by serialLazy(0) // 股票
+    var career by serialLazy(1) // 事业指数
+    var love by serialLazy(1) // 爱情指数
+    var indexHouse by serialLazy(100) // 房价指数
+    var income by serialLazy(2200) // 月净收益
+    var communicationMonthly by serialLazy(0) // 月净交际
+    var happyMonthly by serialLazy(-2) // 月净快乐
 
-    var isStock by serial(false) // 股票 0/1
-    var indexStock by serial(1700) // 股票指数
-    var randomStock by serial(100) // 随机股票指数
+    var isStock by serialLazy(false) // 股票 0/1
+    var indexStock by serialLazy(1700) // 股票指数
+    var randomStock by serialLazy(100) // 随机股票指数
 
-    var healthy by serial(200) // 健康
-    var ability by serial(100) // 能力
-    var experience by serial(100) // 经验
-    var happy by serial(200) // 快乐
-    var morality by serial(200) // 道德
-    var communication by serial(100) // 交际
+    var healthy by serialLazy(200) // 健康
+    var ability by serialLazy(100) // 能力
+    var experience by serialLazy(100) // 经验
+    var happy by serialLazy(200) // 快乐
+    var morality by serialLazy(200) // 道德
+    var communication by serialLazy(100) // 交际
 
-    var isPartner by serial(false) // 伴侣 0/1
-    var isLottery by serial(false) // 彩票 0/1
+    var isPartner by serialLazy(false) // 伴侣 0/1
+    var isLottery by serialLazy(false) // 彩票 0/1
 
-    var yd by serial(0) // YD 30
-    var bl by serial(0) // BL 30
-    var ts by serial(0) // TX 20
-    var py by serial(0) // PY 30
-    var gj by serial(0) // GJ 20
-    var cm by serial(0) // CM 30
-    var cj by serial(0) // CJ 30
-    var zj by serial(0) // ZJ 30
-    var jb by serial(0) // JB 30
-    var sw by serial(0) // SW 20
-    var ty by serial(0) // TY 30
-    var hj by serial(0) // HJ 30
+    var yd by serialLazy(0) // YD 30
+    var bl by serialLazy(0) // BL 30
+    var ts by serialLazy(0) // TX 20
+    var py by serialLazy(0) // PY 30
+    var gj by serialLazy(0) // GJ 20
+    var cm by serialLazy(0) // CM 30
+    var cj by serialLazy(0) // CJ 30
+    var zj by serialLazy(0) // ZJ 30
+    var jb by serialLazy(0) // JB 30
+    var sw by serialLazy(0) // SW 20
+    var ty by serialLazy(0) // TY 30
+    var hj by serialLazy(0) // HJ 30
 
-    var partner by serial(0)
-    var partnerStory by serial(0)
+    var partner by serialLazy(0)
+    var partnerStory by serialLazy(0)
 
-    var partnerXy by serial(0)
-    var partnerZj by serial(0)
-    var partnerSn by serial(0)
-    var partnerSnTime by serial(0)
+    var partnerXy by serialLazy(0)
+    var partnerZj by serialLazy(0)
+    var partnerSn by serialLazy(0)
+    var partnerSnTime by serialLazy(0)
 }
--- app/src/main/java/cn/neday/graduates/repository/Settings.kt
+++ app/src/main/java/cn/neday/graduates/repository/Settings.kt
@@ -2,20 +2,20 @@ package cn.neday.graduates.repository
 
 import cn.neday.graduates.MMKV_Crypt_Key
 import cn.neday.graduates.repository.serialize.MMKVOwner
-import cn.neday.graduates.repository.serialize.serial
+import cn.neday.graduates.repository.serialize.serialLazy
 import com.tencent.mmkv.MMKV
 
 object Settings : MMKVOwner {
     override val kv: MMKV = MMKV.mmkvWithID("settings", MMKV.SINGLE_PROCESS_MODE, MMKV_Crypt_Key)
 
-    var isFirstLaunch by serial(false)
+    var isFirstLaunch by serialLazy(false)
 
-    var isAllowMusicEnable by serial(true)
-    var isAllowSoundEnable by serial(true)
+    var isAllowMusicEnable by serialLazy(true)
+    var isAllowSoundEnable by serialLazy(true)
 
-    var isSponsoredUser by serial(false)
+    var isSponsoredUser by serialLazy(false)
 
-    var isPlaying by serial(false) // 是否游戏进行中
-    var isSave by serial(false)
-    var load by serial(9) // 可读档次数
+    var isPlaying by serialLazy(false) // 是否游戏进行中
+    var isSave by serialLazy(false)
+    var load by serialLazy(9) // 可读档次数
 }
